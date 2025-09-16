b77331623d155a818b5542bc96117d3488693caf
An Lou <louan1734@gmail.com>
2025-09-15 22:10:00 -0500

    start serve2 (api)

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-  res.write("<h1>Hello World!</h1>");
-  res.end();
+const server = http.createServer(async (req, res) => {
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new error("Not Found");
+      }
+
+      const data = await fs.readFile(filePath);
+
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server Error");
+  }
 });
 
 server.listen(8000, () => {
